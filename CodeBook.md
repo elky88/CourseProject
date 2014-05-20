33a177e342cf55cdc58e2386c1011760ab9faf6b
Erin Kelly <elky88@gmail.com>
2014-05-20 13:25:24 -0400

    CodeBook and Initial Script

---
+++ CodeBook.md
@@ -0,0 +1,4 @@
+Getting and Cleaning Data Course Project CodeBook
+=================================================
+
+This codebook will describe the variables, data and any transformations or work I performed to clean up the data.
---
+++ run_analysis.R
@@ -0,0 +1,29 @@
+#1) Merge the training and test sets to create one data set
+
+
+
+
+
+#2) Extract only the measurements on mean and standard deviation for each measurement
+
+
+
+
+
+#3) Use descriptive activity names to name te activities in the data set
+
+
+
+
+
+#4) Appropriately label the data set with the descriptive activity names
+
+
+
+
+
+#5) Create a second, independent tidy data set with the average of each variable for each activity and each subject
+
+
+
+
